dd39b2c01790d047dd42d45bc222be2afd6e99de
wendrul <dethomassin.etienne@gmail.com>
2022-01-12 17:30:47 +0100

    Add rbegin rend

--- vector.hpp
+++ vector.hpp
@@ -97,22 +97,22 @@ namespace me
 
         reverse_iterator rbegin()
         {
-            
+            return reverse_iterator(this.end());
         }
 
         const_reverse_iterator rbegin() const
         {
-
+            return reverse_iterator(this.end());
         }
 
         reverse_iterator rend()
         {
-
+            return reverse_iterator(this.begin());
         }
 
         const_reverse_iterator rend() const
         {
-
+            return reverse_iterator(this.begin());
         }
 #pragma endregion Iterators
 #pragma region Capacity //done
