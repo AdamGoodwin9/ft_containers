742b278159fc59ca375c8691d1aecc56aaba6e3a
Goodywon <goodywon@mutinies.net>
2022-02-12 15:52:42 +0100

    working on vector insert

--- utils.hpp
+++ utils.hpp
@@ -3,6 +3,8 @@
 
 namespace me
 {
+    template<bool Cond, class T = void> struct enable_if {};
+    template<class T> struct enable_if<true, T> { typedef T type; };
     static class nullptr_t
     {
     public:
