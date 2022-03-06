78d517079332b17e4e3be132d554d156d7a06970
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2022-03-06 20:19:51 +0100

    relational operators and external swap

--- utils.hpp
+++ utils.hpp
@@ -201,6 +201,25 @@ namespace ft
 
 #pragma endregion lexicographicalCompare
 
+#pragma region equal
+
+    template <class InputIterator1, class InputIterator2>
+    bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+    {
+        while (first1 != last1)
+        {
+            if (!(*first1 == *first2))
+            {
+                return false;
+            }
+            ++first1;
+            ++first2;
+        }
+        return true;
+    }
+
+#pragma endregion
+
 }
 
 #endif
