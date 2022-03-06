ebebea4714c18cdf01aa196d4167b4a2d40947ba
AdamGoodwin9 <adamgoodwin12345@gmail.com>
2022-03-06 20:31:36 +0100

    added test that fails

--- main.cpp
+++ main.cpp
@@ -96,46 +96,52 @@ void	printReverse(TESTED_NAMESPACE::map<T1, T2> &mp)
 	std::cout << "_______________________________________________" << std::endl;
 }
 
-#define T1 char
-#define T2 int
+#include <list>
+
+#define T1 float
+#define T2 foo<int>
 typedef _pair<const T1, T2> T3;
-
-template <class MAP>
-void	cmp(const MAP &lhs, const MAP &rhs)
-{
-	static int i = 0;
-
-	std::cout << "############### [" << i++ << "] ###############"  << std::endl;
-	std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
-	std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
-	std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
-}
 
 int		test(void)
 {
-	TESTED_NAMESPACE::map<T1, T2> mp1;
-	TESTED_NAMESPACE::map<T1, T2> mp2;
+	std::list<T3> lst;
+	unsigned int lst_size = 5;
+	for (unsigned int i = 0; i < lst_size; ++i)
+		lst.push_back(T3(2.5 + i, i + 1));
 
-	mp1['a'] = 2; mp1['b'] = 3; mp1['c'] = 4; mp1['d'] = 5;
-	mp2['a'] = 2; mp2['b'] = 3; mp2['c'] = 4; mp2['d'] = 5;
+	TESTED_NAMESPACE::map<T1, T2> mp(lst.begin(), lst.end());
+	TESTED_NAMESPACE::map<T1, T2>::iterator it(mp.begin());
+	TESTED_NAMESPACE::map<T1, T2>::const_iterator ite(mp.begin());
+	printSize(mp);
 
-	cmp(mp1, mp1); // 0
-	cmp(mp1, mp2); // 1
+	printPair(++ite);
+	printPair(ite++);
+	printPair(ite++);
+	printPair(++ite);
 
-	mp2['e'] = 6; mp2['f'] = 7; mp2['h'] = 8; mp2['h'] = 9;
+    std::cout << "\n\nA\n\n";
+	it->second.m();
+    std::cout << "\n\nB\n\n";
+	ite->second.m(); //this is broken
+    std::cout << "\n\nC\n\n";
 
-	cmp(mp1, mp2); // 2
-	cmp(mp2, mp1); // 3
+	printPair(++it);
+	printPair(it++);
+	printPair(it++);
+	printPair(++it);
 
-	(++(++mp1.begin()))->second = 42;
+	printPair(--ite);
+	printPair(ite--);
+	printPair(--ite);
+	printPair(ite--);
 
-	cmp(mp1, mp2); // 4
-	cmp(mp2, mp1); // 5
+	(*it).second.m();
+	(*ite).second.m(); //also broken
 
-	swap(mp1, mp2);
-
-	cmp(mp1, mp2); // 6
-	cmp(mp2, mp1); // 7
+	printPair(--it);
+	printPair(it--);
+	printPair(it--);
+	printPair(--it);
 
 	return (0);
 }
